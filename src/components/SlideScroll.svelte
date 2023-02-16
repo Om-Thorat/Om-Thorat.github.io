313db99f8b02856fc0dd18db7c12b44a8d651b81
Om <thoratom1104@gmail.com>
2023-02-16 15:27:18 +0530

    touchevent fix

--- src/components/SlideScroll.svelte
+++ src/components/SlideScroll.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
-
-
+let start:number = 0
 let ScrollContainer:Element
 async function move(){
     document.getElementsByTagName("main")[0].style.opacity = "1"
@@ -9,14 +8,21 @@ async function move(){
         return
     }
     ScrollContainer.addEventListener("wheel", (evt:any) => {
-    evt.preventDefault();
-    ScrollContainer.scrollLeft += evt.deltaY*0.5;
-});
+        evt.preventDefault();
+        ScrollContainer.scrollLeft += evt.deltaY*0.5;
+    });
+    ScrollContainer.addEventListener("touchstart", (evt:any) => {
+        start = evt.touches[0].clientY
+    });
+    ScrollContainer.addEventListener("touchmove",(evt:any) =>{
+        evt.preventDefault();
+        ScrollContainer.scrollLeft += (start - evt.touches[0].clientY)*0.5;
+    });
 }
 onMount(move)
 </script>
 
-<div bind:this={ScrollContainer} class="w-full h-[40%] max-md:landscape:scale-125 md:h-full flex overflow-y-scroll md:overflow-hidden overflow-x-hidden snap-y snap-mandatory md:flex-row flex-col gap-7 z-10 justify-start items-center">
+<div bind:this={ScrollContainer} class="w-full h-[40%] max-md:landscape:scale-125 md:h-full flex overflow-y-scroll md:overflow-hidden overflow-x-auto snap-y snap-mandatory md:flex-row flex-col gap-7 z-10 justify-start items-center">
     <slot />
 </div>
 
