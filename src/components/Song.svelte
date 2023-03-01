bc788b2f80928d8874d0155230db41254c8dae4a
Om <thoratom1104@gmail.com>
2023-03-01 17:47:17 +0530

    oops

--- src/components/Song.svelte
+++ src/components/Song.svelte
@@ -23,10 +23,10 @@
     <div class="absolute flex items-center w-[85%] md:w-auto max-md:landscape:w-auto md:flex-col max-md:landscape:flex-col max-md:landscape:absolute max-md:landscape:justify-center md:absolute md:justify-center">
         <span class="hidden md:block max-md:landscape:text-[1.5vw] max-md:landscape:block w-1/2 font-['Josefin_Sans'] text-[#F1A0CB] text-base tracking-wider md:pb-3 max-md:landscape:pb-[0.6vw] text-center md:w-full max-md:landscape:w-full">{Status}</span>
         <img src="{image}" class=" min-w-[77px] w-[20%] -translate-x-[10%] md:translate-x-0 max-md:landscape:translate-x-0 md:w-[16vw] max-md:landscape:w-[16vw] max-w-[162px] rounded-xl md:rounded-lg max-md:landscape:rounded-lg songimg" alt = "">
-        <div class="flex flex-col items-start md:items-center max-md:landscape:items-center">
+        <div class="flex flex-col items-start mx-2 md:items-center max-md:landscape:items-center">
         <span class="md:hidden max-md:landscape:hidden font-['Josefin_Sans'] text-[#CCFFFF] text-base tracking-wider md:pb-3 max-md:landscape:pb-3 text-center">{Status}</span>
         <span class="font-['Josefin_Sans'] text-[#FCF55C] text-xl max-md:landscape:text-[2vw] max-md:landscape:pt-[2vw] max-md:landscape:pb-[2vw] max-md:landscape:max-h-[9rem] tracking-wider md:pt-4 text-center leading-7 max-h-[4.5rem] overflow-hidden text-ellipsis max-w-[16ch] max-md:landscape:max-w-[12ch] whitespace-nowrap">{Song}</span>
-        <span class="font-['Josefin_Sans'] text-[#F1A0CB] text-lg max-md:landscape:text-[1.6vw] tracking-wider text-center">{Artist}</span>
+        <span class="font-['Josefin_Sans'] text-[#F1A0CB] text-lg max-md:landscape:text-[1.6vw] tracking-wider md:text-center max-w-[16ch] overflow-hidden text-ellipsis max-h-[4rem] text-left">{Artist}</span>
         </div>
     </div>
 </div>
