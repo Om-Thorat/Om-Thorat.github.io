d7632f364b65557ac3ac5b897388b47ad7d5b438
Om <thoratom1104@gmail.com>
2023-03-09 16:29:25 +0530

    buttery if fixed and there is notif

---
+++ src/components/NotifWrap.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let href:string;
+    function send(){
+        console.log(href);
+        window.open(href, '_blank').focus();
+    }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="contents max-sm:hidden" on:click={send}>
+<slot />
+</div>
--- src/components/butterfly.svelte
+++ src/components/butterfly.svelte
@@ -1,86 +1,108 @@
 <script lang="ts">
-    import { onMount } from 'svelte'
-    import {GetDistance,sleep} from "../scripts/fly"
-	let top = -3;
-    let right = -3;
-    let turn = 2.4
-    let sparkle:HTMLElement
-    let anchor:Element
-    let trail:Element
-    let x1 = -3
-    let y1 = -3
+    import {sleep,GetDistance} from '../scripts/fly';
+    import { onMount } from 'svelte';
+    let btrfly:HTMLElement;
+    let anchor:SVGCircleElement;
+    let sparkle:HTMLElement;
+    let trail:HTMLElement;
+    let turn = 0;
+    let left = 0;
+    let ctop = 0;
+    let x1 = 0;
+    let y1 = 0
     let x2 = (95 - Math.floor(Math.random()*20))
     let y2 = (30 - Math.floor(Math.random()*25))
     let x3 = 3
     let y3 = (50 - ((-1)**(Math.floor(Math.random()*2)))*(Math.floor(Math.random()*20)))
     let x4 = 100
     let y4 = 100
+    let t = 0.001
+    let k = 0.003
     let start = false
-    var t = 0.001
-    const k = 0.0025
-    let o = 1
-    async function fly(){
-        if (start == true){
-            o = -o 
-        }   
+    let o = 1;
+    async function fly() {
+        if (start){
+            k = -k;
+            o = -o;
+            return
+        }
         start = true
-        for (let i = 1;i<1000;i++){
-            let TotalX = window.innerHeight*(1.03)
-            let TotalY = window.innerHeight*(1.03)
-            if ((t>1) || (t<0)){
-                t = 0.001
-                break
-            }
-            top = ((1-t)**3)*x1 + (3*(1-t)**2)*t*x2 + (3*(1-t))*t*t*x3 + (t**3)*x4
-            right = ((1-t)**3)*y1 + (3*(1-t)**2)*t*y2 + (3*(1-t))*t*t*y3 + (t**3)*y4
-            var dx = x1*(-3+6*t-3*t**2)+x2*(3-12*t+9*t**2)+x3*(6*t-9*t**2)+3*x4*t**2
-            dx = TotalX*dx
-            var dy = y1*(-3+6*t-3*t**2)+y2*(3-12*t+9*t**2)+y3*(6*t-9*t**2)+3*y4*t**2
-            dy = TotalY*dy
-            turn = (Math.PI/2)+(Math.atan2(dy,dx))
-            t = t +o*(k*Math.abs(turn)+0.001)
-            await sleep(80)
-            if ((trail.children.length <2) || (GetDistance(trail.children[trail.children.length-1],anchor) > 10 )){
-                var coord = anchor.getBoundingClientRect()
-                var newsparkle:HTMLElement = document.importNode(sparkle,true)
-                newsparkle.setAttribute("id",`${t}`)
-                newsparkle.style.position = "fixed"
-                newsparkle.style.visibility = "visible"
-                newsparkle.style.transition = "none"
-                newsparkle.style.top = `${(coord.top)}px`
-                newsparkle.style.left = `${(coord.left)}px`
-                trail.append(newsparkle)
-                if (trail.children.length > 10){
-                    trail.children[0].remove()
-                }
+        var TotalX = window.innerWidth;
+        var TotalY = window.innerHeight;
+        for(let i = 5;i<1000;i++){
+        if ((t>1) || (t<0)){
+            break
+        }
+        // console.log(t)
+        var x = ((1-t)**3)*x1 + (3*(1-t)**2)*t*x2 + (3*(1-t))*t*t*x3 + (t**3)*x4
+        var y = ((1-t)**3)*y1 + (3*(1-t)**2)*t*y2 + (3*(1-t))*t*t*y3 + (t**3)*y4
+        var dx = x1*(-3+6*t-3*t**2)+x2*(3-12*t+9*t**2)+x3*(6*t-9*t**2)+3*x4*t**2
+        dx = TotalX*dx
+        var dy = y1*(-3+6*t-3*t**2)+y2*(3-12*t+9*t**2)+y3*(6*t-9*t**2)+3*y4*t**2
+        dy = TotalY*dy
+        turn = (Math.atan2(dy,dx))
+        console.log(turn,TotalX,TotalY)
+        t = t + k*Math.abs((turn)) + o*0.001
+        btrfly.style.rotate = `${(Math.atan2(dy,dx))}rad`
+        btrfly.style.left = `${x}%`
+        btrfly.style.top = `${y}%`
+        await sleep(40)
+        if ((((trail.children.length) < 2)) || ((GetDistance(trail.children[trail.children.length-1],anchor)) > 10)){
+            var coord = anchor.getBoundingClientRect();
+            var newsparkle = document.importNode(sparkle,true)
+            newsparkle.setAttribute("id",`${t}`)
+            newsparkle.style.position = "absolute"
+            newsparkle.style.visibility = ""
+            newsparkle.style.top = `${coord.top}px`
+            newsparkle.style.left = `${coord.left}px`
+            trail.append(newsparkle)
+            if (trail.children.length >5){
+                trail.children[0].remove()
             }
         }
-        t = 0.001
     }
-    async function waitabitbro() {
-        await sleep(2000)
-        fly()
-    }
-    onMount(waitabitbro)
+}
+async function wait() {
+    await sleep(500)
+}
+onMount(fly)
 </script>
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div class="z-0 scale-75 md:scale-100 flex fixed transition-all duration-[80ms] ease-linear w-[60px] h-[64px]" style="top: max({top}%,-3%);right: max({right}%,-3%);rotate:{turn}rad">
-<div class="w-[10px] h-[60px] flex float-right">
-<svg width="10" height = "60" viewBox="0 0 10 60" class="dot transition-all duration-[80ms] ease-in fixed" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="5" cy="29" r="2.5" bind:this={anchor} fill="#fbcb59" id="anchor" class="fixed transition-all duration-[80ms]" style="visibility: hidden ;"></circle>
-</svg>
-<img src="/sparklesirl.svg" bind:this={sparkle} alt="sparkles" width="10" height="10" class="dot transition-all ease-linear duration-[80ms] fade fixed" style="visibility: hidden;">
-</div>
-<img src="/Butterfly.gif" width="60px" height="64px" class="fly fixed transition-all duration-[80ms] ease-linear" alt="">
-</div>
-<div bind:this={trail}>
 
+<div bind:this={btrfly} style="
+display: flex;
+position: absolute;
+transition: all 40ms linear 0s;
+z-index: 10;
+width: 60px;
+height: 64px;
+left: {left}%;
+top: {ctop}%;
+"
+>
+<div style="width: 10px;height: 60px;display: flex;">
+<svg width="10" height="60" viewBox="0 0 10 60" fill="none" style="transition: transform 40ms ease-in;position: absolute;" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="5" cy="30" r="2.5" fill="#fbcb59" bind:this={anchor} style="visibility: hidden;"></circle>
+</svg>
+<img src="/sparklesirl.svg" alt="" bind:this={sparkle} width="10" height="10" class="fade" style="fill=#FFF;transition: transform 40ms ease-in;position: absolute;visibility: hidden;">
 </div>
+<img src="/Butterfly.gif" alt="Butterfly" bind:this={btrfly} height="64px" width="60px" class="h-[64px] w-[60px]" style="transition: all 40ms linear 0s; z-index: 10;position: absolute;">
+</div>
+<div bind:this={trail}></div>
+
 <style>
     @keyframes fadeout {
-    from {opacity: 100%;}
-    to {opacity: 0%;}
+        from {opacity: 100%;}
+        to {opacity: 0%;}
     }
+    @keyframes move {
+        to {transform: translate(0px,0px);}
+    }
+    @keyframes fadeinout {
+        0% {opacity: 0%;}
+        50% {opacity: 100%;}
+        100% {opacity: 0%;}
+    }
+
     .fade{
         animation: fadeout 900ms ease-out 100ms 1 forwards;
     }
