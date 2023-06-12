f11b0b8a1c01756413e55db0dc1ccdd264fbeedd
Om <thoratom1104@gmail.com>
2023-06-12 18:13:34 +0530

    tooltip

---
+++ src/components/Post.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let cls:string;
+    let d:HTMLElement;
+    function showtip(){
+        console.log(d.children[0].children[0])
+        document.getElementById("tip").innerText = d.children[0].children[0].getAttribute('alt') ?? "The Wall";
+    }
+</script>
+
+<div class="absolute {cls}" bind:this={d} on:mouseenter={showtip} on:mouseleave={()=>{document.getElementById("tip").innerText = "The Wall"}}>
+    <slot></slot>
+</div>
